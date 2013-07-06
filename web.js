a298c2f8dabe318faefdac7a6a3c04c318a932e9
dmitrypodkuiko <dpodkuik@gmail.com>
2013-07-06 05:52:36 +0000

    web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var responseFile=new Buf();
+  responseFile=fs.readFileSync('index.html');
+  response.send(responseFile.toString());
 });
 
 var port = process.env.PORT || 5000;
