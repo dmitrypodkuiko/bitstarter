b2d97894f3d3693f23b8f0336a312818e9d25ee0
dmitrypodkuiko <dpodkuik@gmail.com>
2013-07-06 05:58:18 +0000

    fixing fs reference and removing buffer declaration

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var responseFile=new Buf();
-  responseFile=fs.readFileSync('index.html');
+  var responseFile=fs.readFileSync('index.html');
   response.send(responseFile.toString());
 });
 
